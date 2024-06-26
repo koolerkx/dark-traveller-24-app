05d256501fa1aed5717c672fc2d5e85264e9cae5
Kooler FAN <koolerkx@gmail.com>
2024-06-27 02:42:23 +0800

    Fix onScan useCallback update value
    refs DT-32

--- src/pages/Scan.tsx
+++ src/pages/Scan.tsx
@@ -39,115 +39,118 @@ const Scan: React.FC = () => {
     setIsCameraActive(false);
   });
 
-  const onScan = useCallback(async (result: string) => {
-    if (isLoading) return;
-    if (!authedUser?.email) return;
+  const onScan = useCallback(
+    async (result: string) => {
+      if (isLoading) return;
+      if (!authedUser?.email) return;
 
-    setIsLoading(true);
-    setIsCameraActive(false);
-    presentLoading({
-      message: "請稍候片刻...",
-    });
+      setIsLoading(true);
+      setIsCameraActive(false);
+      presentLoading({
+        message: "請稍候片刻...",
+      });
 
-    const key = {
-      point: "pt",
-      upgrade: "up",
-    };
+      const key = {
+        point: "pt",
+        upgrade: "up",
+      };
 
-    const urlObj = new URL(result);
-    const params = new URLSearchParams(urlObj.search);
-    const pointParam = params.get(key.point);
-    const upgradeParam = params.get(key.upgrade);
+      const urlObj = new URL(result);
+      const params = new URLSearchParams(urlObj.search);
+      const pointParam = params.get(key.point);
+      const upgradeParam = params.get(key.upgrade);
 
-    let resetOnDismiss = false;
+      let resetOnDismiss = false;
 
-    try {
-      if (!userRepository) throw new Error("userRepository not found");
-      const user = await userRepository.getUser(authedUser.email);
+      try {
+        if (!userRepository) throw new Error("userRepository not found");
+        const user = await userRepository.getUser(authedUser.email);
 
-      if (!!pointParam) {
-        const capturedPoint = await userRepository.capturePoint(
-          user,
-          pointParam
-        );
+        if (!!pointParam) {
+          const capturedPoint = await userRepository.capturePoint(
+            user,
+            pointParam
+          );
 
+          resetOnDismiss = true;
+
+          await presentAlert({
+            header: `佔領成功！`,
+            subHeader: `佔領了【${capturedPoint.pointName}】`,
+            buttons: ["關閉訊息"],
+            onDidDismiss: () => {
+              router.push("/profile", "none", "push");
+            },
+          });
+        } else if (!!upgradeParam) {
+          await userRepository.upgradeUser(user, upgradeParam);
+
+          resetOnDismiss = true;
+
+          await presentAlert({
+            header: `升級成功！`,
+            buttons: ["關閉訊息"],
+            onDidDismiss: () => {
+              router.push("/profile", "none", "push");
+            },
+          });
+        }
+      } catch (error) {
         resetOnDismiss = true;
 
-        await presentAlert({
-          header: `佔領成功！`,
-          subHeader: `佔領了【${capturedPoint.pointName}】`,
-          buttons: ["關閉訊息"],
-          onDidDismiss: () => {
-            router.push("/profile", "none", "push");
-          },
-        });
-      } else if (!!upgradeParam) {
-        await userRepository.upgradeUser(user, upgradeParam);
+        if (error instanceof CapturedPointInCooldownError) {
+          presentAlert({
+            header: "攻擊點處於保護狀態",
+            subHeader: `剩餘時間：${error.secondsSincecaptured} / ${CAPTURED_POINT_COOLDOWN_SECONDS}秒`,
+            message: `攻擊點被佔領後5分鐘內，無法被佔領、升級及清除。你可以先到其他攻擊點進行佔領！`,
+            buttons: ["關閉訊息"],
+            onDidDismiss: () => {
+              setIsCameraActive(true);
+              setIsLoading(false);
+            },
+          });
+        } else if (error instanceof UpgradePointAlreadyAppliedError) {
+          presentAlert({
+            header: "已經升級了",
+            message: `你已經在這個升級點進行升級，你可以尋找其他升級點進行升級！`,
+            buttons: ["關閉訊息"],
+            onDidDismiss: () => {
+              setIsCameraActive(true);
+              setIsLoading(false);
+            },
+          });
+        } else if (error instanceof CapturedPointAlreadyCapturedError) {
+          presentAlert({
+            header: "已經佔領了",
+            message: `你已經佔領了這個攻擊點，升級後再佔領！或者先到其他攻擊點進行佔領！`,
+            buttons: ["關閉訊息"],
+            onDidDismiss: () => {
+              setIsCameraActive(true);
+              setIsLoading(false);
+            },
+          });
+        } else {
+          presentAlert({
+            header: "出現錯誤",
+            message: `請稍後再試，數次都不成功請聯絡管理員。`,
+            buttons: ["關閉訊息"],
+            onDidDismiss: () => {
+              setIsCameraActive(true);
+              setIsLoading(false);
+            },
+          });
+        }
 
-        resetOnDismiss = true;
-
-        await presentAlert({
-          header: `升級成功！`,
-          buttons: ["關閉訊息"],
-          onDidDismiss: () => {
-            router.push("/profile", "none", "push");
-          },
-        });
+        console.error(error);
+      } finally {
+        dismissLoading();
+        if (resetOnDismiss) return;
+        setIsCameraActive(true);
+        setIsLoading(false);
       }
-    } catch (error) {
-      resetOnDismiss = true;
-
-      if (error instanceof CapturedPointInCooldownError) {
-        presentAlert({
-          header: "攻擊點處於保護狀態",
-          subHeader: `剩餘時間：${error.secondsSincecaptured} / ${CAPTURED_POINT_COOLDOWN_SECONDS}秒`,
-          message: `攻擊點被佔領後5分鐘內，無法被佔領、升級及清除。你可以先到其他攻擊點進行佔領！`,
-          buttons: ["關閉訊息"],
-          onDidDismiss: () => {
-            setIsCameraActive(true);
-            setIsLoading(false);
-          },
-        });
-      } else if (error instanceof UpgradePointAlreadyAppliedError) {
-        presentAlert({
-          header: "已經升級了",
-          message: `你已經在這個升級點進行升級，你可以尋找其他升級點進行升級！`,
-          buttons: ["關閉訊息"],
-          onDidDismiss: () => {
-            setIsCameraActive(true);
-            setIsLoading(false);
-          },
-        });
-      } else if (error instanceof CapturedPointAlreadyCapturedError) {
-        presentAlert({
-          header: "已經佔領了",
-          message: `你已經佔領了這個攻擊點，升級後再佔領！或者先到其他攻擊點進行佔領！`,
-          buttons: ["關閉訊息"],
-          onDidDismiss: () => {
-            setIsCameraActive(true);
-            setIsLoading(false);
-          },
-        });
-      } else {
-        presentAlert({
-          header: "出現錯誤",
-          message: `請稍後再試，數次都不成功請聯絡管理員。`,
-          buttons: ["關閉訊息"],
-          onDidDismiss: () => {
-            setIsCameraActive(true);
-            setIsLoading(false);
-          },
-        });
-      }
-
-      console.error(error);
-    } finally {
-      dismissLoading();
-      if (resetOnDismiss) return;
-      setIsCameraActive(true);
-      setIsLoading(false);
-    }
-  }, []);
+    },
+    [userRepository, authedUser, router]
+  );
 
   return (
     <IonPage>
